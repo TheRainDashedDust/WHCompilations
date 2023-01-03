bd1c23e1097956eb201302a7f72465a9421fc853
TheRainDashedDust <1366200734@qq.com>
2023-01-03 14:13:44 +0800

    关于五虎项目自动导航功能的代码汇总

---
+++ AINAV/AI_BaseCombat.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Games.AI_Logic
+{
+    public class AI_BaseCombat : BaseAI
+    {
+        public AI_BaseCombat()
+        {
+            //m_AIType = CharacterDefine.AI_TYPE.AI_TYPE_COMBAT;
+            //AIStateType = CharacterDefine.AI_STATE_TYPE.AI_STATE_COMBAT;
+        }
+      
+
+        //void Start()
+        //{
+          
+        //}
+
+        public override void UpdateAI()
+        {
+            base.UpdateAI();
+           
+           
+
+            
+        }
+    }
+}
